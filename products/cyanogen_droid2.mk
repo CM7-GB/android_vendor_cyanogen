2d33f01fcd06d40058f1a54cecc55833403a25d8
Justin E. Goodell <jegoodell@gmail.com>
2011-09-29 01:38:13 -0400

    bump

--- products/cyanogen_droid2.mk
+++ products/cyanogen_droid2.mk
@@ -22,10 +22,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-DROID2
+            ro.modversion=CyanogenMod-7.1.0-DROID2
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-DROID2-KANG
+            ro.modversion=CyanogenMod-7.1.0-DROID2-KANG
     endif
 endif
 
--- products/cyanogen_shadow.mk
+++ products/cyanogen_shadow.mk
@@ -22,10 +22,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-DROIDX
+            ro.modversion=CyanogenMod-7.1.0-DROIDX
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC0-DROIDX-KANG
+            ro.modversion=CyanogenMod-7.1.0-DROIDX-KANG
     endif
 endif
 
